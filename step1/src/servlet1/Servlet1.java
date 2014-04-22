62be0a90e7da95c0eef490293350437651578c4a
Jérémy Longin <jeremy.longin01@gmail.com>
2014-04-22 08:54:13 +0200

    Init files

---
+++ step1/src/servlet1/Servlet1.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import db.DB;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private DB db;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() { super();
+￼
+db=new DB();
+}
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        // TODO Lors de l’appel de http get utiliser les classes precedement
+        // creer pour recuperer la liste des utilisateurs
+        // TODO ecrire la liste des utilisateurs dans le flux de reponse
+        // HttpServletResponse
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+    }
+}
